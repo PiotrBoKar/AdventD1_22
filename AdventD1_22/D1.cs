efa16187e60d7ccc38930fb0779d80943defbe09
piotr <piotr@DESKTOP-8AS28RG>
2022-12-04 22:34:26 +0100

    Dodaj pliki projektów.

---
+++ AdventD1_22/D1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+
+namespace AdventD1_22
+{
+    public class dayOne22
+    {
+        public static void Main()
+        {
+            int sumCalories = 0;
+            int topThree = 0;
+            List<int> caloriesList = ReadFile.getNumArray("22D1");
+            List<int> calories = new List<int>();
+            List<int> topCalories = new();
+            for (int i = 0; i < caloriesList.Count; i++)
+            {
+                if (caloriesList[i] > 0)
+                {
+                    if(i == caloriesList.Count - 1)
+                    {
+                        sumCalories = sumCalories + caloriesList[i];
+                        calories.Add(sumCalories);
+                    }
+                    sumCalories = sumCalories + caloriesList[i];
+                }
+                else if (caloriesList[i] == 0)
+                {
+                    calories.Add(sumCalories);
+                    sumCalories = 0;
+                }
+
+            }
+            var max = calories.Max();
+            Console.WriteLine("-----answer to the first part---------");
+            Console.WriteLine(max);
+            for (int i = 0; i < 3; i++)
+            {
+                topThree = topThree + calories.Max();
+                topCalories.Add(calories.Max());
+                calories.Remove(calories.Max());
+
+            }
+            Console.WriteLine("-----answer to the second part---------");
+            Console.WriteLine(topThree);
+        }
+
+    }
+}
---
+++ AdventD1_22/ReadFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventD1_22
+{
+    internal class ReadFile
+    {
+        public static List<int> getNumArray(string dName)
+        {
+            string path = @"c:\AdventOfCode\" + dName + ".txt";
+            string[] text = File.ReadAllLines(path);
+            List<int> values = new List<int>(text.Length);
+            foreach (string line in text)
+            {
+                int value;
+                if (int.TryParse(line, out value))
+                {
+                    values.Add(value);
+                }
+                else
+                {
+                    values.Add(0);
+                }
+            }
+            return values;
+        }
+    }
+}
+
